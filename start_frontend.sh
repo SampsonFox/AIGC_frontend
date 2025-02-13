73744f7eb9be339034325ed9f3114157765eb421
SampsonFox <sanpsonfox@gmail.com>
2025-02-13 21:02:11 +0800

    set demo

--- start_frontend.sh
+++ start_frontend.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#gnome-terminal -t "pypy3" -x bash -c "pypy3;exec bash;"
-#{
-#	echo "Lunching aigc frontend"
-#	cd /frontend/codes/
-#
-#}
-sh
+# gnome-terminal -t "pypy3" -x bash -c "pypy3;exec bash;"
+{
+	echo "Lunching aigc frontend"
+	cd /frontend/codes/AIGC_frontend/AIGC_frontend/antdx-aigc/
+	npm run start
+}
 wait
